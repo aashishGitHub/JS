67c7b70d1487a03be168021a07e2f947e5af096a
Aashish Kumar <aashish123pqr@gmail.com>
2018-08-21 08:44:15 +0530

    update

--- Design Pattern/pub-sub.js
+++ Design Pattern/pub-sub.js
@@ -23,3 +23,49 @@ var events = {
 		}
 };
 
+-------------------------------OR in production ------------------------------------------
+(function () {
+    'use strict';
+
+    var asis = window.asis;
+
+    asis.PubSub = function () {
+        ///<summary>Events pub/sub manager</summary>
+
+        var _events = {};
+
+        this.subscribe = function (eventName, handler) {
+            _events[eventName] = _events[eventName] || [];
+            _events[eventName].push(handler);
+        };
+
+        this.unsubscribe = function (eventName, handler) {
+            var i = 0,
+                handlers = _events[eventName];
+
+            if (handlers) {
+                while (i < handlers.length) {
+                    if (handlers[i] === handler) {
+                        handlers.splice(i, 1);
+                    } else {
+                        i++;
+                    }
+                }
+            }
+        };
+
+        this.publish = function (eventName) {
+            var i,
+                args,
+                handlers = _events[eventName];
+
+            if (handlers && handlers.length > 0) {
+                args = Array.prototype.slice.call(arguments, 1);
+                for (i = 0; i < handlers.length; i++) {
+                    handlers[i].apply(null, args);
+                }
+            }
+        };
+    };
+
+}());
